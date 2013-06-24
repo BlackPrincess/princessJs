a0109c4eba1bb3141e56187d9feae553d32804de
toshiki.kurokawa <toshiki.kurokawa@jp.oro.com>
2013-06-24 17:03:46 +0900

    Stringのプロトタイプ拡張メソッドを追加

--- src/extend/string.extend.js
+++ src/extend/string.extend.js
@@ -1,31 +1,56 @@
 String.prototype.format = function(arg) {
-	var rep_fn;
-	var str = this;
-	
-	if (typeof arg == "object") {
-		// "{name}".format({name:"name"})
-		rep_fn = function(a, b) { return arg[b] ? arg[b] : a; }
-	} else {
-		// "{0}".format("name");
-		var args = arguments;
-		var argLen = args.length - 1;
-		// {{0}} は {0} で出力させる。置き換えない
-		str = str.replace(/(?!\{)*\{\{(\w+)\}\}/g, function(a, b) {
-			args[++argLen] = a.replace("{{", "{").replace("}}", "}"); 
-			var ret = "{" + argLen + "}";
-			return ret;
-		});
-		rep_fn = function(a, b) { return args[ parseInt(b, 10) ]; }
-	}
-	
+    var rep_fn;
+    var str = this;
+    
+    if (typeof arg == "object") {
+        // "{name}".format({name:"name"})
+        rep_fn = function(a, b) { return arg[b] ? arg[b] : a; }
+    } else {
+        // "{0}".format("name");
+        var args = arguments;
+        var argLen = args.length - 1;
+        // {{0}} は {0} で出力させる。置き換えない
+        str = str.replace(/(?!\{)*\{\{(\w+)\}\}/g, function(a, b) {
+            args[++argLen] = a.replace("{{", "{").replace("}}", "}"); 
+            var ret = "{" + argLen + "}";
+            return ret;
+        });
+        rep_fn = function(a, b) { return args[ parseInt(b, 10) ]; }
+    }
+    
     return str.replace( /(?!\{)*\{(\w+)\}/g, rep_fn );
 };
 
-String.prototype.toHalfNumber = function() {
-    return this.replace(/[０１２３４５６７８９]/g, function(a){
-        var b = "０１２３４５６７８９".indexOf(a);
-        return (b !== -1) ? b : a;
-    }).replace(/\d+\．\d+/g, function(a) {
-        return a.replace("．", ".");
-    });
-};
+(function(){
+    var charCode = 0xFEE0;
+    /**
+     * 全角数字を半角数字へと変換します
+     * @returns {string} 全角数字が半角数字へと変換した文字列
+     */
+    String.prototype.toHalfNumber = function() {
+        return this.replace(/[０１２３４５６７８９]/g, function(a){
+            return String.fromCharCode(a.charCodeAt(0) - charCode);
+        }).replace(/\d+\．\d+/g, function(a) {
+            return a.replace("．", ".");
+        });
+    };
+
+    /**
+     * 全角アルファベットを半角アルファベットへ変換します
+     * @returns {string} 全角アルファベットを半角アルファベットへと変換した文字列
+     */
+    String.prototype.toHalfAlphabet = function() {
+        return this.replace(/[Ａ-Ｚａ-ｚ]/g, function(s){
+            return String.fromCharCode(s.charCodeAt(0) - charCode);
+        });
+    };
+})();
+/**
+ * 全角文字を取り除いた文字列を返します
+ * @returns {string} 全角文字を取り除いた文字列
+ */
+String.prototype.removeFullWidth = function() {
+    return this.replace(/[^!-~]/g, "");
+}
+
+
--- test/extend/string.extend.js
+++ test/extend/string.extend.js
@@ -5,34 +5,45 @@ TestCase("Test String Extension",{
         assertString(actual);
         assertEquals("Foo is Foo.Bar is Bar", actual);
     },
-	"test String.prototype.format {{0}}": function(){
-		var actual = "{0} is Foo.{{0}} is Bar".format("Foo");
+    "test String.prototype.format {{0}}": function(){
+        var actual = "{0} is Foo.{{0}} is Bar".format("Foo");
         assertString(actual);
         assertEquals("Foo is Foo.{0} is Bar", actual);
-	},
-	
-	"test String.prototype.format arguments is object": function(){
-		var actual = "{foo} is Foo.{bar} is Bar".format({foo:"Foo",bar:"Bar"});
+    },
+    
+    "test String.prototype.format arguments is object": function(){
+        var actual = "{foo} is Foo.{bar} is Bar".format({foo:"Foo",bar:"Bar"});
         assertString(actual);
         assertEquals("Foo is Foo.Bar is Bar", actual);
-	},
-	
-	"test String.prototype.format arguments is object(2)": function(){
-		var actual = "{foo} is Foo.{bar} is Bar".format({foo:"Foo"});
+    },
+    
+    "test String.prototype.format arguments is object(2)": function(){
+        var actual = "{foo} is Foo.{bar} is Bar".format({foo:"Foo"});
         assertString(actual);
         assertEquals("Foo is Foo.{bar} is Bar", actual);
-	},
+    },
 
-	"test String.prototype.toHalfNumber" : function() {
-		// TODO:JsTestDriverで実行できるように対応
-		// var actual = "０．２".toNumberHalf();
-		// assertString(actual);
-		// assertEquals("0.2", actual);	// ブラウザ上の実行結果とテストの結果が違う…？
-	},
-	"test String.prototype.toHalfNumber (2)" : function() {
-		// TODO:JsTestDriverで実行できるように対応
-		// var actual = "私．さん".toNumberHalf();
-		// assertString(actual);
-		// assertEquals("私．さん", actual);	// ブラウザ上の実行結果とテストの結果が違う…？
-	}
+    "test String.prototype.toHalfNumber" : function() {
+        // TODO:JsTestDriverで実行できるように対応
+        // var actual = "０．２".toHalfNumber();
+        // assertString(actual);
+        // assertEquals("0.2", actual);    // ブラウザ上の実行結果とテストの結果が違う…？
+    },
+    "test String.prototype.toHalfNumber (2)" : function() {
+        // TODO:JsTestDriverで実行できるように対応
+        // var actual = "私．さん".toHalfNumber();
+        // assertString(actual);
+        // assertEquals("私．さん", actual);    // ブラウザ上の実行結果とテストの結果が違う…？
+    },
+    "test String.prototype.toHalfAlphabet" : function() {
+        // TODO:JsTestDriverで実行できるように対応
+//        var actual = "Ａ-Ｚａ-ｚ".toHalfAlphabet();
+//        assertString(actual);
+//        assertEquals("A-Za-Z", actual);
+    },
+    "test String.prototype.removeFullWidth" : function() {
+        var target = "a" + String.fromCharCode(128);
+        var actual = target.removeFullWidth();
+        assertEquals("a", actual);
+    }
 });
